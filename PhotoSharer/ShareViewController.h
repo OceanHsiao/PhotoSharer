eba74b33382928303a8420c60afc5bf37e27211f
OceanHsiao <oceanhsiao@gmail.com>
2016-10-09 05:35:48 -0400

    init project

---
+++ PhotoSharer/ShareViewController.h
@@ -0,0 +1,13 @@
+//
+//  ShareViewController.h
+//  PhotoSharer
+//
+//  Created by Ocean on 2016-10-09.
+//  Copyright © 2016 Ocean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ShareViewController : UIViewController
+
+@end
---
+++ PhotoSharer/ShareViewController.m
@@ -0,0 +1,13 @@
+//
+//  ShareViewController.m
+//  PhotoSharer
+//
+//  Created by Ocean on 2016-10-09.
+//  Copyright © 2016 Ocean. All rights reserved.
+//
+
+#import "ShareViewController.h"
+
+@implementation ShareViewController
+
+@end
